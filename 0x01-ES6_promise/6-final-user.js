74f713e7ab661602d17aaff63d0b9f52ef20cf29
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-04-09 18:39:58 +0000

    Wrote and exported a function named handleProfileSignup. It should accept three arguments firstName (string), lastName (string), and fileName (string).

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,14 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.all([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((values) => values.map((value) => ({
+      status: 'fulfilled',
+      value,
+    })))
+    .catch((error) => [{
+      status: 'rejected',
+      value: error,
+    }]);
+}
