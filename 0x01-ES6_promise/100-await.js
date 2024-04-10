524d2d420b5b1fdeded083f8b69023ed485375f1
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-04-10 01:46:56 +0000

    Wrote an async function named asyncUploadUser that will call these two functions and return an object with the following format.

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,9 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
 
   try {
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
     return { photo, user };
   } catch (err) {
     return { photo: null, user: null };
