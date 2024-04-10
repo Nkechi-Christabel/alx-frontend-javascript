e0e8121961fd94857579a663e59a7f630efc4343
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-04-10 20:35:12 +0000

    Updated HolbertonCourse.

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,8 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = typeof name === 'string' ? name : '';
-    this._length = typeof length === 'number' ? length : 0;
-    this._students = Array.isArray(students) ? students : [];
+    this._name = typeof name === 'string' && name;
+    this._length = typeof length === 'number' && length;
+    this._students = Array.isArray(students) && students;
   }
 
   get name() {
