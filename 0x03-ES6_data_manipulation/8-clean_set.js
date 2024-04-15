2bf68fd05795fe1eadfd8a908c6f083e86048465
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-04-15 23:32:59 +0000

    Created a function named cleanSet that returns a string of all the set values that start with a specific string (startString).

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  return (startString && startString !== '')
+  return (set && startString && startString !== '')
     ? [...set].filter((str) => str.match(/^(bon)/))
       .map((word) => word.split(startString)[1]).join('-') : '';
 }
