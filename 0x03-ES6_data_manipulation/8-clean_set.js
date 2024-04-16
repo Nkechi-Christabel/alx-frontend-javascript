659253060a9775075c6ebac606cc2e603964d350
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-04-16 19:19:02 +0000

    Updsted cleanSet function.

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  return (startString && startString !== '')
-    ? [...set].filter((str) => (str ? str.startsWith(startString) : ''))
+  return (startString && string.length !== 0)
+    ? [...set].filter((str) => str && str.startsWith(startString))
       .map((word) => word.substring(startString.length)).join('-') : '';
 }
